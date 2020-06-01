23e624bb4f668243bbaf179db76c3b0d73f59ad2
ArraygenR <66153555+ArraygenR@users.noreply.github.com>
2020-06-01 15:52:34 +0530

    Update test.R

--- R/test.R
+++ R/test.R
@@ -1,7 +1,7 @@
 # for testing its genarted
 library(reticulate)
 call_python_code = function(){
-  source_python("../Python/feature_Selection1.py")
+  source_python("Python/feature_Selection1.py")
   test()
 
 }
