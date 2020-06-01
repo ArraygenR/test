1a90b9f7fec9476a13c8cba5e4679ebb0ce164c1
ArraygenR <66153555+ArraygenR@users.noreply.github.com>
2020-06-01 15:24:40 +0530

    Add files via upload

---
+++ Python/feature_Selection1.py
@@ -0,0 +1,17 @@
+def test():
+	import pandas as pd
+	import numpy as np
+	from sklearn.feature_selection import SelectKBest
+	from sklearn.feature_selection import chi2
+	import tqdm
+	data = pd.read_csv("Python/test.csv")
+	X = data.iloc[:,0:20]  #independent columns
+	y = data.iloc[:,-1]    #target column i.e price range#apply SelectKBest class to extract top 10 best features
+	bestfeatures = SelectKBest(score_func=chi2, k=10)
+	fit = bestfeatures.fit(X,y)
+	dfscores = pd.DataFrame(fit.scores_)
+	dfcolumns = pd.DataFrame(X.columns)
+	#concat two dataframes for better visualization 
+	featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+	featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+	print(featureScores.nlargest(15,'Score'))  #print 10 best features
